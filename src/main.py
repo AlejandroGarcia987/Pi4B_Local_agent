d706cae638a629eeea9cc29f63354b945f78d163
AlejandroGarcia987 <alexgdelafuente@gmail.com>
2025-12-08 09:55:11 +0100

    WIP

--- src/main.py
+++ src/main.py
@@ -1,11 +1,51 @@
 import sys 
 import platform
+import logging
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from src.config import settings
+
+
+# Logging basic config
+
+# Basic Logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# Telegram Handlers
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """/start."""
+    user = update.effective_user
+    await update.message.reply_html(
+        f"Hello {user.mention_html()}! I am your Local Agent, ready for service.",
+    )
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echoes user message"""
+    await update.message.reply_text(f"Received message: '{update.message.text}'. Processing...")
 
 def main():
+
+    # Init
     print("--- Local Agent System Info ---")
     print(f"OS: {platform.system()} {platform.release()}")
     print(f"Python Version: {sys.version}")
-    print("Code end")
+
+    # Load token
+    token = settings.TELEGRAM_BOT_TOKEN
+
+    # Init and bot config
+    application = Application.builder().token(token).build()
+
+    #Suscribe handlers
+    application.add_handler(CommandHandler("start", start_command))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    logger.info("Agent is starting to poll for updates...")
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
