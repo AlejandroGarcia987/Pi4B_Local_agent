9ac821dd48ac0d006271d9980fbc69a783605d02
AlejandroGarcia987 <alexgdelafuente@gmail.com>
2025-12-08 16:28:10 +0100

    Pydantic fix

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
-from pydantic import BaseSettings, SettingsConfigDict
 from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     #Pydantic config to load form .env
