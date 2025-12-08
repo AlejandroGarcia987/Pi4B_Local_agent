7684af15565b30da3b84320e787e8152cad3a57d
AlejandroGarcia987 <alexgdelafuente@gmail.com>
2025-12-08 10:06:52 +0100

    fix: update src/config.py imports to comply with pydantic-settings V2

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
-from pydantic import BaseSettings, ConfigDict
+from pydantic import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
 class Settings(BaseSettings):
     #Pydantic config to load form .env
-    model_config = ConfigDict(env_file='.env', env_file_config='utf-8')
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
     #Variables
     TELEGRAM_BOT_TOKEN: str = Field(..., description="Telegram Bot Token. Must be loaded from .env")
